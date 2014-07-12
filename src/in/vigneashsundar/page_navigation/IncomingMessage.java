2839250f83f10be7380fe7d06056d2e34a067442
vikene <vigneashsundar@live.com>
2014-07-12 09:03:29 +0530

    Added Notification Builder == > Incomming message Indication 	Added notification manager ==> Incomming message Management 	Not modified Toast notification 	Intent yet to add

--- src/in/vigneashsundar/page_navigation/IncomingMessage.java
+++ src/in/vigneashsundar/page_navigation/IncomingMessage.java
@@ -6,20 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.app.Activity;
 
 public class IncomingMessage extends BroadcastReceiver {
 
+		private NotificationManager mmanager;
 	@Override
 	public void onReceive(Context arg0, Intent arg1) {
 		// TODO Auto-generated method stub
 		// Retrieves a map of extended data from the intent.
+		
         final Bundle bundle = arg1.getExtras();
  
         try {
              
+        		
             if (bundle != null) {
                  
                 final Object[] pdusObj = (Object[]) bundle.get("pdus");
+                NotificationCompat.Builder build = new NotificationCompat.Builder(arg0);
+                build.setSmallIcon(R.drawable.ic_launcher);
+                
                  
                 for (int i = 0; i < pdusObj.length; i++) {
                      
@@ -30,7 +41,14 @@ public class IncomingMessage extends BroadcastReceiver {
                     String message = currentMessage.getDisplayMessageBody();
  
                     
+                     build.setContentTitle("Javaji: "+phoneNumber);
+                     build.setContentText(message);
                      
+                     NotificationManager mNotificationManager =
+                    		    (NotificationManager) arg0.getSystemService(Context.NOTIFICATION_SERVICE);
+                    		    
+                    		// notificationID allows you to update the notification later on.
+                    		mNotificationManager.notify("1", 0, build.build());
  
                    // Show Alert
                     int duration = Toast.LENGTH_LONG;
@@ -47,5 +65,6 @@ public class IncomingMessage extends BroadcastReceiver {
         }
 		
 	}
+	 
 
 }
